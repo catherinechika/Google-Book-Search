c14147764f640ae4137f39e94de1bfed5345e629
Chika <catherine.achukwu@gmail.com>
2019-09-21 13:21:28 -0400

    Jumbotron added from Bootstrap

---
+++ googlebooks/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import react from "react"
+function Jumbotron() {
+    render(
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Fluid jumbotron</h1>
+                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ googlebooks/src/pages/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+import NavBar from "../components/NavBar";
+
+function Search() {
+    return (
+        <div>
+            <NavBar />
+            <Jumbotron />
+            Search
+            </div>
+    );
+}
+
+export default Search;
