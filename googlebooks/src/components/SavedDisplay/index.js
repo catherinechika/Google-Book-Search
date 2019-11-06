2fbfc2e35be3f5591a66d096e0e4b4b0deb06fad
Chika <catherine.achukwu@gmail.com>
2019-11-06 15:33:02 -0500

    mont function bug fix

--- googlebooks/src/components/SavedDisplay/index.js
+++ googlebooks/src/components/SavedDisplay/index.js
@@ -1,28 +1,32 @@
-import React from "react"
+import React, {Component} from "react"
 import axios from "axios";
- class SavedDisplay extends components{
 
+
+
+ class SavedDisplay extends Component{
  state = {
      bookList : [], 
-     refreshPage: false
+     refreshed: false
  }
 
  componentDidMount() {
     axios
         .get('/api/books')
-        .then(res => this.setState({ bookList: res.data }))
+        .then(res => this.setState({ 
+            bookList: res.data, 
+            refreshed: true }))
         .catch(err => console.log(err));
 }
 
-  deleteBook(book){
-axios.delete("api/books", book).then(refreshPage())
- }
+//   deleteBook(book){
+// axios.delete("api/books", book).then(refreshPage())
+//  }
 
 
-render (props) {
+render () {
     return (
         <div className="card">
-            {props.results.map(results => (
+            {this.state.bookList.map(results => (
                 <div className='text' key={results._id}>
                     <h5 className="card-header">{results.title}</h5>
                     <div className="card-body">
@@ -31,7 +35,7 @@ render (props) {
                     </div>
 
                     <a href={results.infoLink} target="_blank" rel="noopener noreferrer" className="btn btn-outline-success">See More</a>
-                    <button className="btn btn-outline-dark" onClick={() => { deleteBook(results) }}>DELETE</button>
+                    <button className="btn btn-outline-dark" >DELETE</button>
                 </div>
             ))}
 
